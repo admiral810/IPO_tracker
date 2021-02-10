4b6693c0c86aac63c3c9910e12d834571975888c
Adam Roberts <adamjamesroberts@gmail.com>
2021-02-09 19:27:34 -0600

    added scripts to schedule tweets

---
+++ tweet_stock_live_today.py
@@ -0,0 +1,161 @@
+# import libraries
+import requests
+import time
+from datetime import datetime, date, timedelta
+import json
+import sys
+import random
+import numpy as np
+import pandas as pd
+
+import matplotlib.pyplot as plt
+from pandas.plotting import table
+
+import seaborn as sns
+colors = ["#118AB2", "#EF476F", "#FFD166", "#06D6A0", "#EE754D", "#002E99"]
+
+import plotly.figure_factory as ff
+
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+# mysql login info
+sys.path.insert(0, '../../Key')
+from mysql_secret import dbuser, dbpass, dbhost, dbname
+engine = create_engine(f'mysql://{dbuser}:{dbpass}@{dbhost}/{dbname}?charset=utf8')
+
+# Establish Twitter connection
+import tweepy
+
+# get tweepy credentials
+sys.path.insert(0, '../../Key')
+from tweepy_keys import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+
+random_goes_live_today = [
+    "see what they're about below",
+    "below are the #things they #do",
+    "here's some more info",
+    "#read about them below",
+    "what are they about? just look below...",
+    "who the heck are they? this is the heck that they are...",
+    "wanna learn more?",
+    "are you gonna buy them or not buy them?", 
+    "might buy them or might not...",
+    "this is financial advise (just kidding)",
+    "some things you should know...",
+    "beep bop I'm a robot this is what I know about them",
+    "here's what they do",
+    "check em out",
+    "who cares (if you do here's more on them)",
+    "write up about who they are below",
+    "this is what their business does", 
+    "this is what they do",
+    "ok more is below if you care if you don't thats fine too", 
+    "thanks for stopping by here's a lil more on them below",
+    "learn more below"
+    ]
+
+
+random_end_of_desc = [
+    "yada yada yada you get it",
+    "ok look if you're still reading go to their website",
+    "I think we get the point",
+    "you're getting bored that's enough for now",
+    "blah blahhhh blah blahhhh blah we get it #amiright",
+    "well that's most of it",
+    "ok goofballs that's enough for now",
+    "a solid mix of interesting and not interesting!", 
+    "okey doke I got it",
+    "you get the picture",
+    "and then there's some more stuff but I digress",
+    "makes sense to me by now",
+    "ok... got it",
+    "neat I guess",
+    "sounds unique or not unique depends who you talk to",
+    "omg WE GET IT",
+    "so that's what they do", 
+    "enough story time on them for now",
+    "ugh who cares", 
+    "Robot needs a rest that's enough info for now",
+]
+
+# get todays date
+today = datetime.today().strftime("%Y-%m-%d")
+
+# get stocks that go live today
+engine = create_engine(f'mysql://{dbuser}:{dbpass}@{dbhost}/{dbname}?charset=utf8')
+connection = engine.connect()
+df = pd.read_sql(f"""
+    SELECT s.*, 
+        ci.city, 
+        ci.state,
+        ci.country,
+        ci.website, 
+        ci.industry,
+        ci.sector, 
+        ci.business_summary
+    FROM stocks s
+    LEFT JOIN company_info ci
+        ON s.symbol = ci.symbol
+    WHERE priced_date = '{today}'
+        AND exchange <> 'NASDAQ Capital'""", connection)
+connection.close()
+df['region'] = np.where(df['country']=='United States', df['city'] + ", " + df['state'], 
+                        df['city'] + ", " + df['country'])
+
+# filter out SPACS
+df = df.loc[(df["exchange"] != 'NASDAQ Capital')]
+df = df.loc[~(df["company"].str.contains("Acquisition", regex=False))]  #any company that contains "Acquisition"
+
+# keep only ones that have region and industry
+df = df.dropna(subset=['region', 'industry'])
+
+row_count = len(df.index)
+
+if row_count > 0:
+    for row in df.itertuples():
+        
+        symbol = row.symbol
+        company = row.company
+        industry = row.industry.lower()
+        region = row.region
+        business_summary = "\n\n\n" + row.business_summary
+        business_summary.replace('\\n', '\n')
+        
+        # get tweet text
+        tweet_text = f"{company} ${symbol} should trade today, a {industry} company out of {region} ... {random.choice(random_goes_live_today)}"
+        
+        # text for image of company description
+        description = business_summary + " " + random.choice(random_end_of_desc)
+        
+        # company description image
+        fig = plt.figure(figsize=(6,4))
+        ax = fig.add_subplot(111)
+        ax.axis([0, 10, 0, 10])
+        ax.text(5, 10, description, fontsize=12, style='normal', ha='center',
+                 va='top', wrap=True, bbox={'facecolor': 'none', 'edgecolor': 'none', 'alpha': 0.2, 'pad': 5})
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.axes.xaxis.set_visible(False)
+        ax.axes.yaxis.set_visible(False)
+        plt.savefig('comp_desc.png')
+        plt.show
+
+        # the name of the media file 
+        filename = "comp_desc.png"
+
+        # upload the file 
+        media = api.media_upload(filename) 
+        
+        # Send out the tweet
+        api.update_with_media(filename="comp_desc.png", media_id=media.media_id, status=tweet_text)
+        
+        time.sleep(240)
---
+++ tweet_upcoming_ipos.py
@@ -0,0 +1,173 @@
+# import libraries
+import requests
+import time
+from datetime import datetime, date, timedelta
+import json
+import sys
+import random
+import numpy as np
+import pandas as pd
+
+# matplotlib
+import matplotlib.pyplot as plt
+from pandas.plotting import table
+
+# seaborn
+import seaborn as sns
+colors = ["#118AB2", "#EF476F", "#FFD166", "#06D6A0", "#EE754D", "#002E99"]
+
+# plotly  https://plotly.com/python/figure-factory-table/
+import plotly.figure_factory as ff
+
+# sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+
+# mysql login info
+sys.path.insert(0, '../../Key')
+from mysql_secret import dbuser, dbpass, dbhost, dbname
+engine = create_engine(f'mysql://{dbuser}:{dbpass}@{dbhost}/{dbname}?charset=utf8')
+
+# Establish Twitter connection
+import tweepy
+
+# get tweepy credentials
+sys.path.insert(0, '../../Key')
+from tweepy_keys import CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+random_upcoming_ipo = [
+    "check em out I guess",
+    "I dunno who cares",
+    "if you care",
+    "are these good I dunno",
+    "whats the point",
+    "are any good?",
+    "worth buying?",
+    "are they overvalued",
+    "any kewl ones?",
+    "if you wanna dip a toe into em",
+    "could be good or duds", 
+    "this week",
+    "... neat",
+    "#stonks lmao right?",
+    "from my robot",
+    "coming at ya",
+    "we'll see I guess", 
+    "just cuz",
+    "per the internet",
+    "beep bop Im a robot",
+    "schedule accordingly",
+    "pass... or don't up to you",
+    "at least thats the plan", 
+    "have a good week",
+    "kewl kewl kewl",
+    "might buy all or none or some",
+    "thanks for stopping by"
+    ]
+
+def remove_comp_suffix(df, company='company'):
+    """Removes traling company name info that takes up space."""
+    remove_text = [' /FL', '/DE', ', Inc.', ' Inc.', ', INC.', ' INC.',' PLC', ', Corp.', 'corp.', ' Corp.', ' CORP.', 
+                   ' CORP',', Ltd', ' Ltd', ' plc',  ' PLC', ' SE']
+    for t in remove_text:
+        df[company] = df[company].str.rstrip(t)
+    
+    return df    
+
+
+######################################################
+# CONSTRUCT AND SEND TWEET
+######################################################
+
+# get range for today until one week from today
+today = date.today()
+week_from_today = today + timedelta(days=7)
+
+#convert to string for query purposes from SQL
+today = today.strftime("%Y-%m-%d")
+week_from_today = week_from_today.strftime("%Y-%m-%d")
+
+print(f"Range is {today} to {week_from_today}")
+
+# get upcoming IPOs
+connection = engine.connect()
+tw_ipos_df = pd.read_sql(f"""
+    SELECT *
+    FROM stocks
+    WHERE priced_date BETWEEN '{today}' and '{week_from_today}'
+    """, connection)
+
+# convert to datetime
+tw_ipos_df['priced_date'] =  pd.to_datetime(tw_ipos_df['priced_date'])
+
+# add column for month day
+tw_ipos_df["price_month_day"] = tw_ipos_df['priced_date'].dt.strftime('%b %d')
+
+# add market cap
+tw_ipos_df["market_cap"] = (tw_ipos_df['dollar_val_shares'].astype(float)/1000000).round(1).astype(str) + 'M'
+
+# filter out SPACS
+tw_ipos_df = tw_ipos_df.loc[(tw_ipos_df["exchange"] != 'NASDAQ Capital')]
+tw_ipos_df = tw_ipos_df.loc[~(tw_ipos_df["company"].str.contains("Acquisition", regex=False))]  #any company that contains "Acquisition"
+
+tw_ipos_df = tw_ipos_df.sort_values("priced_date")
+
+# remove inc. and corp. and stuff like that from company name
+tw_ipos_df = remove_comp_suffix(tw_ipos_df)
+
+###########################
+# GET TWEET TEXT 
+###########################
+tweet_text = f"Upcoming IPOs {random.choice(random_upcoming_ipo)}\n"
+
+for row in tw_ipos_df.itertuples():
+    
+    # remove .00 from proposed share prices to reduce unecesary characters
+    psp = row.proposed_share_price.replace(".00", "")
+    
+    # if going to exceed the length of twitter characters allowed then break out of loop
+    if len(tweet_text) > 245:
+        break
+    else:
+        tweet_text += "\n"
+        row_text = f"{row.price_month_day}: ${row.symbol} at ${psp}" 
+        tweet_text += row_text
+print(tweet_text)
+
+###########################
+# GET TWEET IMAGE 
+###########################
+
+# table for tweet image
+tw_ipos_img_df = tw_ipos_df[['price_month_day', 'symbol', 'company', 'proposed_share_price', 'market_cap']]
+tw_ipos_img_df['proposed_share_price'] = "$" + tw_ipos_img_df['proposed_share_price']
+tw_ipos_img_df = tw_ipos_img_df.rename(columns={'price_month_day': 'Date',
+                                                'symbol': 'Symbol',
+                                                'company': 'Company',
+                                                'proposed_share_price': 'Proposed Price',
+                                                'market_cap': 'Market Cap'
+                                               })
+
+# get tweet image
+colorscale = [[0, '#118AB2'],[.5, '#FFFFFF'],[1, '#F2F2F2']]
+fig =  ff.create_table(tw_ipos_img_df, colorscale=colorscale, height_constant=20)
+
+fig.write_image("weekly_ipos.png", scale = 1)
+
+###########################
+# SEND TWEET 
+###########################
+
+# the name of the media file 
+filename = "weekly_ipos.png"
+
+# upload the file 
+media = api.media_upload(filename) 
+
+# Send out the tweet
+api.update_with_media(filename="weekly_ipos.png", media_id=media.media_id, status=tweet_text)
